89ff94da93de7dbf6ee9316353a0679006ecd6cf
Tiago Ribeiro <tribeiro@lsst.org>
2025-03-11 13:34:44 -0700

    Add documentation header for the package.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! Manage observing environment for the Vera Rubin Observatory Control System.
+//!
+#![doc = include_str!("../CHANGELOG.md")]
+
 #[macro_use]
 extern crate serde_derive;
 pub mod error;
